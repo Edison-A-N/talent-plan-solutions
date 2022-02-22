f6b6a4093af0e437d86eade6633d1b86451165d4
Edison <Edison.A.N@hotmail.com>
2022-02-22 17:35:19 +0800

    fix: sstable test

--- courses/rust/solutions/project-2/tests/sstable_test.rs
+++ courses/rust/solutions/project-2/tests/sstable_test.rs
@@ -1,34 +1,30 @@
-use kvs::sstable::SSTable;
 use kvs::sstable;
-use std::{path::PathBuf, fs};
+use kvs::sstable::SSTable;
+use std::{fs, path::PathBuf};
 
 static TMP_LOG: &str = "/tmp/test_log_dir.sst";
 
-fn clear(){
+fn clear() {
     fs::remove_file(PathBuf::from(&TMP_LOG)).expect("remove file error");
 }
 
 #[test]
 fn new_sstable() {
-    let _ = SSTable::new(
-        PathBuf::from(&TMP_LOG));
+    let _ = SSTable::new(PathBuf::from(&TMP_LOG));
 }
 
 #[test]
 fn load_sstable() {
-    let mut sstable = SSTable::new(
-        PathBuf::from(&TMP_LOG));
+    let mut sstable = SSTable::new(PathBuf::from(&TMP_LOG));
     sstable.load();
 }
 
 #[test]
 fn write_sstable() {
-    // clear();
-    let mut sstable = SSTable::new(
-        PathBuf::from(&TMP_LOG));
+    clear();
+    let mut sstable = SSTable::new(PathBuf::from(&TMP_LOG));
     sstable.load();
-    let mut sstcolumn = sstable::SSTColumn::new(sstable::ActionMap::Set, 
-        "1".to_owned(), "1".to_owned());
+    let sstcolumn =
+        sstable::SSTColumn::new(sstable::ActionMap::Set, "1".to_owned(), "1".to_owned());
     sstable.write_ahead(sstcolumn).expect("write ahead error");
 }
-
